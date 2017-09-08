Made fixes on ConvCacheIterator;  Added batch normalization for VGG model.
Made fixes on ConvCacheIterator;  Added batch normalization for VGG
model.

common/model/deeplearning/imagerec/optimization/ConvCacheIterator.py
@@ -39,14 +39,12 @@ class ConvCacheIterator(Iterator):
         self.CACHE_FILE_QUEUE = deque(maxlen=self.FILE_QUEUE_SIZE)
         self.__start_file_load_daemon_threads()
         self.__advance_to_next_cache_file()
-        num_entries_in_file = self.__get_num_entries_in_current_file()
-        super(ConvCacheIterator, self).__init__(num_entries_in_file, batch_size=batch_size, shuffle=shuffle, seed=seed)
+        super(ConvCacheIterator, self).__init__(0, batch_size=batch_size, shuffle=shuffle, seed=seed)
 
     def next(self):
         with self.lock:
             index_array, current_index, current_batch_size = next(self.index_generator)
-
-        return self.__get_batch_data_from_cache(index_array)
+            return self.__get_batch_data_from_cache(index_array)
 
     def __start_file_load_daemon_threads(self):
         for thread_num in range(self.FILE_QUEUE_SIZE):
@@ -58,27 +56,29 @@ class ConvCacheIterator(Iterator):
             time.sleep(0.01)
 
     def _flow_index(self, n, batch_size=32, shuffle=False, seed=None):
-        # Ensure self.batch_index is 0.
-        self.reset()
         while 1:
             num_entries_in_file = self.__get_num_entries_in_current_file()
-            self.n = num_entries_in_file
 
-            if seed is not None:
-                np.random.seed(seed + self.total_batches_seen)
-            if self.batch_index == 0:
-                index_array = np.arange(num_entries_in_file)
-                if shuffle:
-                    index_array = np.random.permutation(n)
 
             current_index = (self.batch_index * batch_size) % num_entries_in_file
             if num_entries_in_file > current_index + batch_size:
                 current_batch_size = batch_size
                 self.batch_index += 1
             else:
-                current_batch_size = num_entries_in_file - current_index
                 self.batch_index = 0
                 self.__advance_to_next_cache_file()
+                num_entries_in_file = self.__get_num_entries_in_current_file()
+                current_batch_size = num_entries_in_file - current_index
+
+            self.n = num_entries_in_file
+
+            if seed is not None:
+                np.random.seed(seed + self.total_batches_seen)
+
+            if shuffle:
+                index_array = np.random.permutation(num_entries_in_file)
+            else:
+                index_array = np.arange(num_entries_in_file)
 
             self.total_batches_seen += 1
             yield (index_array[current_index: current_index + current_batch_size],
@@ -129,7 +129,6 @@ class ConvCacheIterator(Iterator):
 
     def __get_num_entries_in_current_file(self):
         return len(self.CURRENT_FEATURE_ARRAY)
-
 
     def __advance_to_next_cache_file(self):
         while len(self.CACHE_FILE_QUEUE) == 0:

common/model/deeplearning/imagerec/pretrained/vgg16.py
@@ -8,6 +8,7 @@ import re
 import keras
 import numpy as np
 from keras import layers
+from keras.layers import BatchNormalization
 from keras.layers.convolutional import MaxPooling2D, ZeroPadding2D, Conv2D
 from keras.layers.core import Flatten, Dense, Dropout, Lambda
 from keras.models import Sequential
@@ -90,8 +91,10 @@ class Vgg16(IImageRecModel):
         return [
             Flatten(),
             Dense(4096, activation='relu'),
+            BatchNormalization(),
             Dropout(drop_out),
             Dense(4096, activation='relu'),
+            BatchNormalization(),
             Dropout(drop_out),
             Dense(num_classes, activation='softmax')
         ]
@@ -108,7 +111,7 @@ class Vgg16(IImageRecModel):
         for dense_layer in dense_layers:
             model.add(dense_layer)
 
-        model.load_weights(get_file('vgg16.h5', self.ORIGINAL_MODEL_WEIGHTS_URL + 'vgg16.h5', cache_subdir='models'))
+        model.load_weights(get_file('vgg16_bn.h5', self.ORIGINAL_MODEL_WEIGHTS_URL + 'vgg16_bn.h5', cache_subdir='models'))
         return model
 
     def __initialize_model(self):
@@ -285,7 +288,7 @@ class Vgg16(IImageRecModel):
             conv_cache_training_batches = ConvCacheIterator(cache_directory=conv_cache_directory, batches=batches,
                     batch_id = 'training', conv_model=self.conv_model_portion, batch_size=self.TRAINING_BATCH_SIZE, shuffle=True)
             conv_cache_validation_batches = ConvCacheIterator(cache_directory=conv_cache_directory, batches=val_batches,
-                    batch_id = 'validation', conv_model=self.conv_model_portion, batch_size=self.VALIDATION_BATCH_SIZE, shuffle=True)
+                    batch_id = 'validation', conv_model=self.conv_model_portion, batch_size=self.VALIDATION_BATCH_SIZE, shuffle=False)
 
             self.dense_model_portion.fit_generator(conv_cache_training_batches, steps_per_epoch=steps_per_epoch, epochs=nb_epoch, initial_epoch=initial_epoch,
                                      validation_data=conv_cache_validation_batches, validation_steps=int(np.ceil(val_batches.samples / self.VALIDATION_BATCH_SIZE)),